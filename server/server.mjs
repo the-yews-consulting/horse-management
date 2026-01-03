413b38736bbce9274a9110544baed43ce6d242e6
the-yews-consulting <82463829+the-yews-consulting@users.noreply.github.com>
2026-01-03 17:32:06 +0000

    Updated server.mjs

--- server/server.mjs
+++ server/server.mjs
@@ -40,7 +40,7 @@ app.use('/api/ha-integration', haIntegrationRoutes);
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
